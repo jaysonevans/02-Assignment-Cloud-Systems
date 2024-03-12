9a48566a7f28b1bfd9ff17f2b73b69bd29759a75
Jayson Evans <evansjay@sheridancollege.ca>
2024-03-12 18:46:12 -0400

    Add main skeleton

---
+++ main.tf
@@ -0,0 +1,16 @@
+# main.tf
+# Jayson Evans
+# March 12, 2024
+# Orchestrate modules
+
+module "vpc" {
+    source = "./vpc"
+}
+
+module "security group" {
+    source = "./security-group"
+}
+
+module "ec2" {
+    source = "./ec2"
+}
